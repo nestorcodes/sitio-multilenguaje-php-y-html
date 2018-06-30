8d6e2639705614db8e67703af993d8a5c618554d
nestorcodes <40641750+nestorcodes@users.noreply.github.com>
2018-06-30 12:04:51 -0500

    Add files via upload

---
+++ lenguaje/en.php
@@ -0,0 +1,9 @@
+<?php
+
+$lenguaje = array(
+	"titulo" => "Multilanguage Site",
+	"header" => "My First Multilanguage Site",
+	"bienvenida" => "Welcome to the first Multilanguage site",
+	"parrafo" => "Hi, I hope this little demonstration of a Multilanguage site will help you",
+	"cambio" => "change language",
+);
---
+++ lenguaje/es.php
@@ -0,0 +1,9 @@
+<?php
+
+$lenguaje = array(
+	"titulo" => "Sitio Multilenguaje",
+	"header" => "Primer Sitio Multilenguaje",
+	"bienvenida" => "Bienvenidos a primer sitio Multilenguaje",
+	"parrafo" => "Hola, espero te sirva esta pequeña demostración de un sitio Multilenguaje",
+	"cambio" => "cambiar idioma",
+);
